850cdd9038664131046b3fc8e2b57a2a6984c5b8
mohamedelkashif <mohamed_elkashif@windowslive.com>
2021-01-08 17:13:12 +0200

    adding main function logic

--- src/main/java/com/gameofthree/GameOfThreeServer.java
+++ src/main/java/com/gameofthree/GameOfThreeServer.java
@@ -1,16 +1,25 @@
 package com.gameofthree;
 
+import com.gameofthree.server.GameServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.ServerSocket;
 
 public class GameOfThreeServer {
     private static final Logger LOGGER = LoggerFactory.getLogger(GameOfThreeServer.class);
 
-    public static void main(String [] args){
+    private static final String PORT = "8080";
+    public static void main(String [] args) throws IOException {
 
         LOGGER.info("The game has been started");
 
         // main logic here
+        try (GameServer gameServer = new GameServer(Integer.parseInt(PORT))){
+            ServerSocket serverSocket = gameServer.start();
+
+        }
 
         LOGGER.info("The Game has been ended / closed");
     }
