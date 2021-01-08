b87e4cda3923dfdcbe8c1fc3dd2b08a9fe818fef
mohamedelkashif <mohamed_elkashif@windowslive.com>
2021-01-08 16:37:54 +0200

    adding the main entry point to the application

---
+++ src/main/java/com/gameofthree/GameOfThreeServer.java
@@ -0,0 +1,17 @@
+package com.gameofthree;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GameOfThreeServer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(GameOfThreeServer.class);
+
+    public static void main(String [] args){
+
+        LOGGER.info("The game has been started");
+
+        // main logic here
+
+        LOGGER.info("The Game has been ended / closed");
+    }
+}
