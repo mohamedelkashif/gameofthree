2d7981c9ea96c89c73afc8c40531f6f662134394
mohamedelkashif <mohamed_elkashif@windowslive.com>
2021-01-12 11:16:42 +0200

    adding dto for user input and its serializer

--- src/main/java/com/gameofthree/controller/mapper/UserInputDeserializer.java
+++ src/main/java/com/gameofthree/controller/mapper/UserInputDeserializer.java
@@ -1,4 +1,16 @@
 package com.gameofthree.controller.mapper;
 
-public class UserInputDeserializer {
+import com.gameofthree.game.dtos.UserInputDTO;
+
+import java.util.function.Function;
+
+public class UserInputDeserializer implements Function<String, UserInputDTO> {
+    @Override
+    public UserInputDTO apply(String input) {
+        String[] rawUserInputArray = input.split(":", 2);
+        String command = rawUserInputArray[0];
+        String data = rawUserInputArray.length > 1 ? rawUserInputArray[1] : "";
+
+        return new UserInputDTO(command, data);
+    }
 }
--- src/main/java/com/gameofthree/game/dtos/UserInputDTO.java
+++ src/main/java/com/gameofthree/game/dtos/UserInputDTO.java
@@ -1,4 +1,28 @@
 package com.gameofthree.game.dtos;
 
+import java.util.Optional;
+
 public class UserInputDTO {
+
+    private String command;
+    private String data;
+
+    public UserInputDTO(String command, String data) {
+        this.command = command;
+        this.data = data;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public boolean isValid() {
+        return Optional.ofNullable(command)
+                .filter(s -> !s.isEmpty())
+                .isPresent();
+    }
 }
