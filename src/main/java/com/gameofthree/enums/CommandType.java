56f25f46eecf44e1c162d1f9e224814c2100e09e
mohamedelkashif <mohamed_elkashif@windowslive.com>
2021-01-12 11:15:42 +0200

    adding enum for command types

--- src/main/java/com/gameofthree/enums/CommandType.java
+++ src/main/java/com/gameofthree/enums/CommandType.java
@@ -1,4 +1,20 @@
 package com.gameofthree.enums;
 
+import java.util.Arrays;
+
 public enum CommandType {
+    ADD_PLAYER,
+    ADD_MACHINE,
+    START,
+    PLAY,
+    STATE,
+    EXIT,
+    UNKNOWN;
+
+    public static CommandType valueOfString(final String commandTypeStr) {
+        return Arrays.stream(CommandType.values())
+                .filter(commandType -> commandType.toString().equals(commandTypeStr))
+                .findFirst()
+                .orElse(UNKNOWN);
+    }
 }
